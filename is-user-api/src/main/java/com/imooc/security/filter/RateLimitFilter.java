f3a5ed4e85a303a751c739b4616171702da5e939
易成杰 <626659321@qq.com>
2020-07-01 22:52:51 +0800

    适用guava做限流

---
+++ is-user-api/src/main/java/com/imooc/security/filter/RateLimitFilter.java
@@ -0,0 +1,40 @@
+package com.imooc.security.filter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * ClassName: RateLimitFilter
+ * Description: TODO(描述)
+ * Date: 2020/7/1 22:46
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+@Component
+public class RateLimitFilter extends OncePerRequestFilter {
+    // 每秒一个请求的限流器
+    private RateLimiter rateLimiter = RateLimiter.create(1) ;
+
+    protected void doFilterInternal(
+            HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
+            throws ServletException, IOException {
+        if (rateLimiter.tryAcquire()){
+            chain.doFilter(req,resp);
+        }else {
+            resp.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            resp.getWriter().write("to many request!!！");
+            resp.getWriter().flush();
+        }
+    }
+}
