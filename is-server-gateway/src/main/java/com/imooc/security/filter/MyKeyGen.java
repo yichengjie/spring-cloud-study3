d5e19f4f2dbf6824f127f31da90111565f69ed1b
易成杰 <626659321@qq.com>
2020-07-05 16:05:37 +0800

    网关限流扩展点

--- is-server-gateway/src/main/java/com/imooc/security/filter/MyKeyGen.java
+++ is-server-gateway/src/main/java/com/imooc/security/filter/MyKeyGen.java
@@ -1,5 +1,13 @@
 package com.imooc.security.filter;
 
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.RateLimitUtils;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.properties.RateLimitProperties;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.support.DefaultRateLimitKeyGenerator;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * ClassName: MyKeyGen
  * Description: TODO(描述)
@@ -9,5 +17,15 @@ package com.imooc.security.filter;
  * 修改记录
  * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
  */
-public class MyKeyGen {
+@Component
+public class MyKeyGen extends DefaultRateLimitKeyGenerator {
+    public MyKeyGen(RateLimitProperties properties, RateLimitUtils rateLimitUtils) {
+        super(properties, rateLimitUtils);
+    }
+
+    @Override
+    public String key(HttpServletRequest request, Route route, RateLimitProperties.Policy policy) {
+        // 这里可以扩展key的生成
+        return super.key(request, route, policy);
+    }
 }
--- is-server-gateway/src/main/java/com/imooc/security/filter/MyRateLimitErrorHandler.java
+++ is-server-gateway/src/main/java/com/imooc/security/filter/MyRateLimitErrorHandler.java
@@ -1,6 +1,10 @@
 package com.imooc.security.filter;
 
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.DefaultRateLimiterErrorHandler;
+import org.springframework.stereotype.Component;
+
 /**
+ * 限流错误扩展点
  * ClassName: MyRateLimitErrorHandler
  * Description: TODO(描述)
  * Date: 2020/7/5 15:32
@@ -9,5 +13,12 @@ package com.imooc.security.filter;
  * 修改记录
  * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
  */
-public class MyRateLimitErrorHandler {
+@Component
+public class MyRateLimitErrorHandler extends DefaultRateLimiterErrorHandler {
+
+    @Override
+    public void handleError(String msg, Exception e) {
+        // limit限流时扩展点
+        super.handleError(msg, e);
+    }
 }
